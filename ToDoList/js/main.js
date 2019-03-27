30338c508ef5e237b8fb2247ef1aff2fa6ed1b64
raziel0110 <albangabriel42@gmail.com>
2019-03-27 14:05:04 +0200

    to do upgraded

--- ToDoList/js/main.js
+++ ToDoList/js/main.js
@@ -1,3 +1,4 @@
+let values = [];
 $(document).ready(function() {
   populateList();
   $("#button-addon2").click(function() {
@@ -12,23 +13,61 @@ $("input[name=newItem]").on("focus", () => {
 });
 
 $(document).on("click", ".removeElement", function() {
-  const valueItem = $(this)
-    .parent()
-    .text();
-  const newValue = removeFinalLetter(valueItem);
-  removeFromLocalStorage(newValue);
-
-  $(this)
-    .parent()
-    .remove();
+  $("#areyousure").show("fast");
+  const that = $(this);
+  $("#yes").click(function() {
+    const valueItem = that.parent().text();
+    const newValue = removeFinalLetter(valueItem);
+    removeFromLocalStorage(newValue);
+    that.parent().remove();
+    $("#areyousure").hide();
+  });
+  $("#no").click(function() {
+    $("#areyousure").hide();
+  });
 });
 
 $(document).on("dblclick", ".list-element", function() {
   const valueItem = $(this).text();
   const newValue = removeFinalLetter(valueItem);
-  removeFromLocalStorage(newValue);
-  $(this).remove();
+  values.push(newValue);
+  const newTag = $('<div class="input-group list-input-group"></div>');
+  const input = $(
+    '<input type="text" name="inputItem" class="form-control inputValue" aria-description="replace" />'
+  );
+  input.val(newValue);
+  const replace_btn_holder = $('<div class="input-group-append"></div>');
+  const replace_btn = $(
+    '<button class="btn btn-outline-warning replace-btn" type="button">Replace</button>'
+  );
+  replace_btn_holder.append(replace_btn);
+  newTag.append(input);
+  newTag.append(replace_btn_holder);
+  $(this).replaceWith(newTag);
 });
+
+$(document).on("click", ".replace-btn", function() {
+  const newVal = $("input[name=inputItem]").val();
+  const oldVal = values[0];
+  console.log(newVal);
+  console.log(oldVal);
+  $(".list-input-group").replaceWith(replaceValue(newVal));
+  updateLocalStorage(oldVal, newVal);
+  values = [];
+});
+
+function replaceValue(value) {
+  const listElement = $(
+    '<li class="list-group-item d-flex justify-content-between align-items-center list-element"></li>'
+  );
+  const span = $(
+    '<span class="badge badge-danger badge-pill removeElement"></span>'
+  );
+  span.html("x");
+  listElement.html(value);
+  listElement.append(span);
+  $("#list-group").append(listElement);
+}
 
 function getFromLocalStorage() {
   let items;
@@ -39,6 +78,16 @@ function getFromLocalStorage() {
     items = JSON.parse(itemLs);
   }
   return items;
+}
+
+function updateLocalStorage(oldVal, newVal) {
+  let items = getFromLocalStorage();
+  for (let i = 0; i < items.length; i++) {
+    if (oldVal === items[i]) {
+      items[i] = newVal;
+    }
+  }
+  localStorage.setItem("items", JSON.stringify(items));
 }
 
 function addTolocalStorage(item) {
