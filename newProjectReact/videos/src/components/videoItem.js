696fd50eee7fa6821e316549d5630e897dcbdb71
raziel0110 <albangabriel42@gmail.com>
2019-04-22 14:14:35 +0300

    videos finised

--- newProjectReact/videos/src/components/videoItem.js
+++ newProjectReact/videos/src/components/videoItem.js
@@ -3,9 +3,16 @@ import "./videoItem.css";
 
 class VideoItem extends React.Component {
   render() {
+    const { video } = this.props;
     return (
-      <div className="video-item item">
+      <div
+        className="video-item item"
+        onClick={() => {
+          this.props.onVideoSelect(video);
+        }}
+      >
         <img
+          alt={video.snippet.title}
           className="ui image"
           src={this.props.video.snippet.thumbnails.medium.url}
         />
--- newProjectReact/videos/src/components/videoList.js
+++ newProjectReact/videos/src/components/videoList.js
@@ -1,9 +1,15 @@
 import React from "react";
 import VideoItem from "./videoItem";
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderList = videos.map(video => {
-    return <VideoItem video={video} />;
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        video={video}
+        onVideoSelect={onVideoSelect}
+      />
+    );
   });
   return <div className="ui relaxed divided list">{renderList}</div>;
 };
