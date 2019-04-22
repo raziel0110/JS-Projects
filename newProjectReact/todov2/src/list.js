778248bbfefa827ca224f56542d16eec8dc0e921
raziel0110 <albangabriel42@gmail.com>
2019-04-22 15:38:05 +0300

    todov2 update with listItem

--- newProjectReact/todov2/src/list.js
+++ newProjectReact/todov2/src/list.js
@@ -1,29 +1,15 @@
 import React from "react";
 import "./list.css";
+import ListItem from "./listItem";
 
 class List extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.removeTodo = this.removeTodo.bind(this);
-  }
-  removeTodo(item) {
-    // const element = e.target.parentNode.parentNode;
-    // const { items } = this.props;
-    // const obj = items.find(el => {
-    //   return el.id === parseInt(element.getAttribute("data-id"));
-    // });
+  removeTodo = item => {
     this.props.onRemove(item);
-  }
+  };
 
   updateTodo = item => {
-    const value = item.value; // e.target.parentNode.previousSibling.textContent;
-    // const { items } = this.props;
-    // const element = e.target.parentNode.parentNode;
-    const obj = item; /* items.find(el => {
-      return el.id === parseInt(element.getAttribute("data-id"));
-    });*/
-
+    const value = item.value;
+    const obj = item;
     obj.value = prompt("do you wanna modify?", value);
     this.props.onUpdate(obj);
   };
@@ -33,27 +19,12 @@ class List extends React.Component {
       <div className="list-body">
         <ul className="list-ul">
           {this.props.items.map(item => (
-            <li key={item.id}>
-              <div className="item-value">{item.value}</div>
-              <div className="item-delBtn">
-                <button
-                  className="updateBtn"
-                  onClick={() => {
-                    this.updateTodo(item);
-                  }}
-                >
-                  Update
-                </button>
-                <button
-                  className="btnDel"
-                  onClick={() => {
-                    this.removeTodo(item);
-                  }}
-                >
-                  Delete
-                </button>
-              </div>
-            </li>
+            <ListItem
+              item={item}
+              onRemove={this.removeTodo}
+              onUpdate={this.updateTodo}
+              key={item.id}
+            />
           ))}
         </ul>
       </div>
