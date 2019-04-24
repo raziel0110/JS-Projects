36f77058b3dcea0b8e39aeb0dcb89ebb62e7d446
raziel0110 <albangabriel42@gmail.com>
2019-04-24 17:49:54 +0300

    changes

--- newProjectReact/photos-app/src/Components/Modal.js
+++ newProjectReact/photos-app/src/Components/Modal.js
@@ -1,25 +1,27 @@
 import React from "react";
-
+import ReactDOM from "react-dom";
 import "./modal.css";
 
+const modal = document.getElementById("modal");
+
 export default class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.el = document.createElement("div");
+  }
+  componentDidMount() {
+    modal.appendChild(this.el);
+  }
+
+  componentWillUnmount() {
+    modal.removeChild(this.el);
+  }
+
   render() {
-    const { show, src } = this.props;
-    const showHideClaseName = show
-      ? "modal display-block"
-      : "modal display-none";
-    return (
-      <div className={showHideClaseName}>
-        <div className="modal-main">
-          <img
-            src={`https://picsum.photos/id/${src.id}/900`}
-            alt={src.author}
-          />
-          <button className="hideBtn" onClick={this.props.handleClose}>
-            X
-          </button>
-        </div>
-      </div>
-    );
+    // const { show, src } = this.props;
+    // const showHideClaseName = show
+    //   ? "modal display-block"
+    //   : "modal display-none";
+    return ReactDOM.createPortal(this.props.children, this.el);
   }
 }
--- newProjectReact/photos-app/src/Components/image.js
+++ newProjectReact/photos-app/src/Components/image.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Modal from "./Modal";
 import "./image.css";
+import "./modal.css";
 
 export default class Image extends React.Component {
   state = { show: false };
@@ -14,6 +15,28 @@ export default class Image extends React.Component {
   render() {
     const { item } = this.props;
 
+    const modal = this.state.show && (
+      <Modal show={this.state.show}>
+        <div>
+          <div className="modal-main">
+            <img
+              src={`https://picsum.photos/id/${item.id}/${item.width}/${
+                item.height
+              }`}
+              alt={item.author}
+            />
+            <div>
+              <p>{item.author}</p>
+            </div>
+
+            <button className="hideBtn" onClick={this.hideModal}>
+              X
+            </button>
+          </div>
+        </div>
+      </Modal>
+    );
+
     return (
       <div className="image">
         <div>
@@ -22,16 +45,8 @@ export default class Image extends React.Component {
             alt={item.author}
             onClick={this.showModal}
           />
+          {modal}
         </div>
-        <Modal
-          show={this.state.show}
-          handleClose={this.hideModal}
-          src={this.props.item}
-        >
-          <div>
-            <p>{item.author}</p>
-          </div>
-        </Modal>
       </div>
     );
   }
