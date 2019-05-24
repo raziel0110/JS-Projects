407a46288ff023f2dfc7040b4e7e8affa598a561
raziel0110 <albangabriel42@gmail.com>
2019-05-24 16:01:43 +0300

    redux todo styling

--- newProjectReact/counter-redux/src/Todo.js
+++ newProjectReact/counter-redux/src/Todo.js
@@ -1,27 +1,58 @@
 import React from "react";
 import { connect } from "react-redux";
+import "./Todo.css";
 
 const List = props => {
   console.log(props);
   return (
-    <div>
-      <h1>Shoping List</h1>
-      <form onSubmit={props.onSubmit}>
-        <input value={props.inputSearchText} onChange={props.handleChange} />
-      </form>
-      <div>
-        <ul>
-          {props.todos.map((todo, index) => {
-            return (
-              <li key={index}>
-                <div onClick={() => props.onDelete(index)}>{todo}</div>
-                <div>
-                  <button onClick={() => props.onUpdate(index)}>Update</button>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-6">
+          <h3 className="shadow-sm p-3 mb-5 bg-white rounded header-text">
+            Shoping List
+          </h3>
+          <form onSubmit={props.onSubmit}>
+            <div className="form-group">
+              <input
+                className="form-control"
+                value={props.inputSearchText}
+                onChange={props.handleChange}
+                placeholder="Enter item for shop"
+              />
+            </div>
+          </form>
+          <div>
+            <div className="jumbotron items">
+              <p className="length-items">
+                Number of todos:
+                <span className="item-color">{`  ${props.todos.length}`}</span>
+              </p>
+            </div>
+            <ul className="list-group">
+              {props.todos.map((todo, index) => {
+                return (
+                  <li key={index} className="list-group-item list-item">
+                    <div>{todo}</div>
+                    <div>
+                      <button
+                        onClick={() => props.onUpdate(index)}
+                        className="btn btn-outline-success btn-padding"
+                      >
+                        Update
+                      </button>
+                      <button
+                        onClick={() => props.onDelete(index)}
+                        className="btn btn-outline-danger btn-padding"
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
